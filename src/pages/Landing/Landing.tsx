705fcf0bcf930a649d37b774ffbc1e99f32c06f7
FELIPE_AGUIAR <felipeferreiraaguiar9@gmail.com>
2023-10-18 05:13:40 -0400

    no coments about this error

--- src/pages/Landing/Landing.tsx
+++ src/pages/Landing/Landing.tsx
@@ -5,15 +5,18 @@ import { useNavigate } from "react-router-dom";
 const Landing: React.FC = () => {
   const navigate = useNavigate();
 
+  const goClient = () => navigate("client");
+  const goAdmin = () => navigate("admin");
+
   return (
     <div className={styles.main}>
       <h1 className="text-xl font-bold">Escolha seu tipo de usuário</h1>
-      <div className={styles.types} onClick={() => navigate("/client")}>
-        <div className={styles.type}>
+      <div className={styles.types}>
+        <div className={styles.type} onClick={goClient}>
           <FaUsers />
           <span>Cliente</span>
         </div>
-        <div className={styles.type} onClick={() => navigate("/admin")}>
+        <div className={styles.type} onClick={goAdmin}>
           <FaUserLock />
           <span>Admin</span>
         </div>
